30321cda4586faa92173d99b1a7451f784100212
Muhammad AYAN <m101ayan92@gmail.com>
2025-03-22 14:24:41 +0500

    Update main.js

--- main.js
+++ main.js
@@ -1,2 +1,7 @@
 import './style.css'
+import product from "./api/products.json";
+import { showProductContainer } from './productcontainer';
+import { shopproductcontainer } from './shop';
 
+shopproductcontainer(product)
+showProductContainer(product)
