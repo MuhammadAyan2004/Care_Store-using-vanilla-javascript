e52a63dc2c9907a6a8e41273eaf898e854f57914
Muhammad AYAN <m101ayan92@gmail.com>
2025-04-26 15:01:38 +0500

    Update shop.js

--- shop.js
+++ shop.js
@@ -1,4 +1,6 @@
-export const shop_cart = (product)=>{
+import { addtocart } from "./addtocart"
+
+export const shop_cart = (product) => {
     const productcontainer3 = document.querySelector(".productContainer3")
     const productTemps = document.getElementById("productTemp")
     const prebtn = document.querySelector(".pre")
@@ -10,78 +12,80 @@ export const shop_cart = (product)=>{
 
     // firstpage
     const firstPageBtn = document.createElement("button")
-    firstPageBtn.classList.add("n_page","first_page")
+    firstPageBtn.classList.add("n_page", "first_page")
     firstPageBtn.textContent = "1";
     pagesdiv.appendChild(firstPageBtn)
 
     // lastpage
     const lastpagebtn = document.createElement("button")
-    lastpagebtn.classList.add("n_page","last_page")
+    lastpagebtn.classList.add("n_page", "last_page")
     lastpagebtn.textContent = totalPages;
     pagesdiv.appendChild(lastpagebtn)
 
-// dynamically create middle btton    
-function middlePage(){
-    let middleBtn = pagesdiv.querySelector(".n_page:not(.first_page):not(.last_page)");
-    
-    for(let i = 2; i<=totalPages; i++){
-        const pageBtn = document.createElement("button")
-        pageBtn.classList.add("n_page")
-        pageBtn.textContent = i
-        pageBtn.addEventListener("click",()=>{
-            currentPage = i;
-            renderProduct()
+    // dynamically create middle btton    
+    function middlePage() {
+        let middleBtn = pagesdiv.querySelector(".n_page:not(.first_page):not(.last_page)");
+
+        for (let i = 2; i <= totalPages; i++) {
+            const pageBtn = document.createElement("button")
+            pageBtn.classList.add("n_page")
+            pageBtn.textContent = i
+            pageBtn.addEventListener("click", () => {
+                currentPage = i;
+                renderProduct()
+            })
+            pagesdiv.insertBefore(pageBtn, lastpagebtn)
+        }
+        lastpagebtn.remove()
+    }
+    // render products
+    function renderProduct() {
+        productcontainer3.innerHTML = "";
+        let start = (currentPage - 1) * itemsPerPage;
+        let end = start + itemsPerPage;
+        if (!product) {
+            return false;
+        }
+        product.slice(start, end).forEach((currelem) => {
+            const { id, img, name, price, brand, stock, details } = currelem;
+            const tempClone_S = document.importNode(productTemps.content, true)
+
+            tempClone_S.querySelector("#cardvalue").setAttribute("id", `card${id}`);
+            tempClone_S.querySelector(".img").src = img;
+            tempClone_S.querySelector(".brands").innerHTML = brand;
+            tempClone_S.querySelector(".product_name").innerHTML = name;
+            tempClone_S.querySelector(".price").textContent = `$${price}`
+            tempClone_S.querySelector(".addToCart").addEventListener("click", (event) => {
+                addtocart(event, id , stock)
+            })
+            productcontainer3.appendChild(tempClone_S)
         })
-        pagesdiv.insertBefore(pageBtn,lastpagebtn)
     }
-    lastpagebtn.remove()
-}
 
-function renderProduct(){
-    productcontainer3.innerHTML = "";
-    let start = (currentPage - 1) * itemsPerPage;
-    let end = start + itemsPerPage;
-    if(!product){
-        return false;
-    }
-    product.slice(start,end).forEach((currelem)=>{
-        const {id, img, name, price, brand, stock, details} = currelem;
-        const tempClone_S = document.importNode(productTemps.content,true)
-    
-        tempClone_S.querySelector("#cardvalue").setAttribute("id",`card${id}`);
-        tempClone_S.querySelector(".img").src = img;
-        tempClone_S.querySelector(".brands").innerHTML = brand;
-        tempClone_S.querySelector(".product_name").innerHTML = name;
-        tempClone_S.querySelector(".price").textContent = `$${price}`
-    
-        productcontainer3.appendChild(tempClone_S) 
+    nextbtn.addEventListener("click", () => {
+        if (currentPage < totalPages) {
+            currentPage++
+            renderProduct()
+        }
+    });
+
+    prebtn.addEventListener("click", () => {
+        if (currentPage > 1) {
+            currentPage--
+            renderProduct()
+        }
     })
-}
 
-nextbtn.addEventListener("click",()=>{
-    if(currentPage < totalPages){
-        currentPage++
+    firstPageBtn.addEventListener("click", () => {
+        currentPage = 1;
         renderProduct()
-    }
-});
+    });
 
-prebtn.addEventListener("click",()=>{
-    if(currentPage > 1){
-        currentPage--
+    lastpagebtn.addEventListener("click", () => {
+        currentPage = totalPages;
         renderProduct()
-    }
-})
+    });
 
-firstPageBtn.addEventListener("click", () => {
-    currentPage = 1;
+    middlePage()
     renderProduct()
-});
-
-lastpagebtn.addEventListener("click", () => {
-    currentPage = totalPages;
-    renderProduct()
-});
-
-middlePage()
-renderProduct()
 }
